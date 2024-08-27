2bf41c3943eb4f0299e8da23c3a5d6a08597cb1f
tushxr12 <tusharforcourses@gmail.com>
2024-08-27 23:40:41 +0530

    Practice basic string problem

---
+++ Beginner_Problems/Basic_Strings/Longest common prefix.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &str)
+    {
+        // your code goes here
+
+        sort(str.begin(), str.end());
+
+        string x = str[0];
+        string y = str[str.size() - 1];
+
+        string ans = "";
+
+        for (int i = 0; i < max(x.size(), y.size()); i++)
+        {
+            if (x[i] != y[i])
+            {
+                return ans;
+            }
+            else
+            {
+                ans += x[i];
+            }
+        }
+
+        return ans;
+    }
+};
