c5d4ebf2f87822d88e7ced608bc9da33fe60fea8
tushxr12 <tusharforcourses@gmail.com>
2024-09-02 19:13:43 +0530

    Add: Language basics prob

---
+++ Beginner_Problems/Language Basics/Print last character of string.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    char lastChar(string &s)
+    {
+        // your code goes here
+        int n = s.size();
+        char lastCharr = s[n - 1];
+        return lastCharr;
+    }
+};
