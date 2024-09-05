c8023b3f6d5ad7641ed7cce3261febeeeee05cab
tushxr12 <tusharforcourses@gmail.com>
2024-09-05 23:12:17 +0530

    Practice pattern prob 2

---
+++ Beginner_Problems/Patterns/Pattern 2.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    void pattern2(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                cout << "*";
+            cout << "\n";
+        }
+    }
+};
