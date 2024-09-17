e813c509d354cf0b478d3d871006114880ed6266
tushxr12 <tusharforcourses@gmail.com>
2024-09-17 23:35:13 +0530

    Practice recursion FAQ prob

---
+++ Binary Trees/FAQs/LCA in BT.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        // your code goes here
+        if (!root || root == p || root == q)
+            return root;
+
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == NULL)
+            return right;
+        else if (right == NULL)
+            return left;
+        else
+            return root;
+    }
+};
