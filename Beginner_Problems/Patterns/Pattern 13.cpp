ad3b4e5a3509157d8b38a6c1b0f4c7a4c0d2bb02
tushxr12 <tusharforcourses@gmail.com>
2024-09-22 13:56:55 +0530

    Practice pattern prob

---
+++ Beginner_Problems/Patterns/Pattern 13.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    void pattern13(int n)
+    {
+        int num = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                cout << num << " ";
+                num = num + 1;
+            }
+            cout << endl;
+        }
+    }
+};
