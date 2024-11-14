2c1b70ab23ada6516db3464021a2066624533e9c
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-14 23:26:57 +0530

    Add tabulation code

--- Dynamic Programming/1D_DP/Climbing stairs.cpp
+++ Dynamic Programming/1D_DP/Climbing stairs.cpp
@@ -1,3 +1,4 @@
+//Memoization
 class Solution {
 private:
     int func(int n, vector<int> &dp)
@@ -14,5 +15,22 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
         return func(n, dp);
+    }
+};
+
+
+//Add tabulation
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2;i <= n;i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
