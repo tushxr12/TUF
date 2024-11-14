0bf941fa857af17c980640cbc5feaa222d0f3442
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-15 00:07:34 +0530

    Add Tabulation approach

--- Dynamic Programming/1D_DP/Frog Jump.cpp
+++ Dynamic Programming/1D_DP/Frog Jump.cpp
@@ -1,3 +1,4 @@
+//Memoization
 class Solution {
 private:
     int f(int ind, vector<int> &heights, vector<int> &dp)
@@ -23,5 +24,25 @@ public:
         int n = heights.size();
         vector<int> dp(n + 1,-1);
         return f(n - 1, heights, dp);
+    }
+};
+
+//Tabulation
+class Solution {
+public:
+    int frogJump(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> dp(n + 1,-1);
+        dp[0] = 0;
+        for(int i = 1;i < n;i++)
+        {
+            int firstStep = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+            int secondStep = INT_MAX;
+            if(i > 1)
+                secondStep = dp[i - 2] + abs(heights[i] - heights[i - 2]);
+
+            dp[i] = min(firstStep, secondStep);
+        }
+        return dp[n - 1];
     }
 };
