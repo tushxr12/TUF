64d141f7d8fdb99ea9f7478d1454bc630e9c407a
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-14 23:38:30 +0530

    Add Space optimized  approach

--- Dynamic Programming/1D_DP/Climbing stairs.cpp
+++ Dynamic Programming/1D_DP/Climbing stairs.cpp
@@ -34,3 +34,22 @@ public:
         return dp[n];
     }
 };
+
+
+//Add Space Optimized
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for(int i = 2;i <= n;i++)
+        {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
