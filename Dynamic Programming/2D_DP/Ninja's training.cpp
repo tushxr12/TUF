6e82056d6f29a42e42aed8bc02e2121cece49306
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-16 00:10:17 +0530

    Add space optimized approach

--- Dynamic Programming/2D_DP/Ninja's training.cpp
+++ Dynamic Programming/2D_DP/Ninja's training.cpp
@@ -109,3 +109,37 @@ public:
         return dp[n-1][3];
     }
 };
+
+//Space Optimized Approach
+class Solution {
+public:
+    int ninjaTraining(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<int> prev(4, 0);
+        prev[0] = max(matrix[0][1], matrix[0][2]);
+        prev[1] = max(matrix[0][0], matrix[0][2]);
+        prev[2] = max(matrix[0][0], matrix[0][1]);
+        prev[3] = max({matrix[0][0], matrix[0][1], matrix[0][2]});
+
+        for(int day = 1; day < n;day++)
+        {
+            vector<int> temp(4,0);
+            for(int last = 0; last <= 3;last++)
+            {
+                temp[last] = 0;
+
+                for(int task = 0; task <= 2;task++)
+                {
+                    if(task != last)
+                    {
+                        int point = matrix[day][task] + prev[task];
+                        temp[last] = max(temp[last], point);
+                    }
+                }
+            }
+            prev = temp;
+        }
+
+        return prev[3];
+    }
+};
