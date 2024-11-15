fc0511d057a5f359dfccd6b235539ffe82931f02
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-16 00:02:32 +0530

    Add tabulation approach

--- Dynamic Programming/2D_DP/Ninja's training.cpp
+++ Dynamic Programming/2D_DP/Ninja's training.cpp
@@ -77,3 +77,35 @@ public:
         return f(n - 1,3,matrix, dp);
     }
 };
+
+//Tabulation Approach
+class Solution {
+public:
+    int ninjaTraining(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] = max(matrix[0][1], matrix[0][2]);
+        dp[0][1] = max(matrix[0][0], matrix[0][2]);
+        dp[0][2] = max(matrix[0][0], matrix[0][1]);
+        dp[0][3] = max({matrix[0][0], matrix[0][1], matrix[0][2]});
+
+        for(int day = 1; day < n;day++)
+        {
+            for(int last = 0; last <= 3;last++)
+            {
+                dp[day][last] = 0;
+
+                for(int task = 0; task <= 2;task++)
+                {
+                    if(task != last)
+                    {
+                        int point = matrix[day][task] + dp[day-1][task];
+                        dp[day][last] = max(dp[day][last], point);
+                    }
+                }
+            }
+        }
+
+        return dp[n-1][3];
+    }
+};
