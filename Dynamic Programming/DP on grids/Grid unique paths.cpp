4e9d7c161ab5133e16ea41ba3f9314d96ea35daf
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-17 14:29:56 +0530

    Add memoization approach

--- Dynamic Programming/DP on grids/Grid unique paths.cpp
+++ Dynamic Programming/DP on grids/Grid unique paths.cpp
@@ -16,3 +16,25 @@ public:
         return f(n - 1,m - 1);
     }
 };
+
+//Memoization
+class Solution {
+private:
+    int f(int i,int j,vector<vector<int>> &dp)
+    {
+        if(i == 0 && j == 0)
+            return 1;
+        if(i < 0 || j < 0)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int up = f(i - 1,j,dp);
+        int left = f(i, j -1,dp);
+        return dp[i][j] = (up + left);
+    }
+public:
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(n - 1,m - 1,dp);
+    }
+};
