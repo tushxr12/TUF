094780c66e526233dcafc8c6cfc26c5ff2024e01
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-17 15:01:23 +0530

    Add tabulation approach

--- Dynamic Programming/DP on grids/Unique paths II.cpp
+++ Dynamic Programming/DP on grids/Unique paths II.cpp
@@ -49,3 +49,41 @@ public:
         return f(n - 1,m - 1,matrix,dp);
     }
 };
+
+//Tabulation
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        if(matrix[0][0] == 1 || matrix[n - 1][m - 1] == 1)
+            return 0;
+        
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                if(i == 0 && j == 0 && dp[i][j] != 1)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int up = 0, left = 0;
+                if(i > 0 && matrix[i - 1][j] != 1)
+                {
+                    up = dp[i - 1][j];
+                }
+
+                if(j > 0 && matrix[i][j - 1] != 1)
+                {
+                    left = dp[i][j - 1];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
+
