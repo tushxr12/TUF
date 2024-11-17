02964f440c2c938255743c8fcc2c4f81487647d6
Tushar <Tushar.Tushar@ellucian.com>
2024-11-17 14:40:14 +0530

    Add space optimiation approach

--- Dynamic Programming/DP on grids/Grid unique paths.cpp
+++ Dynamic Programming/DP on grids/Grid unique paths.cpp
@@ -78,3 +78,45 @@ public:
         return dp[n - 1][m - 1];
     }
 };
+
+//Space Optimization
+class Solution {
+private:
+    int f(int i,int j,vector<vector<int>> &dp)
+    {
+        if(i == 0 && j == 0)
+            return 1;
+        if(i < 0 || j < 0)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int up = f(i - 1,j,dp);
+        int left = f(i, j -1,dp);
+        return dp[i][j] = (up + left);
+    }
+public:
+    int uniquePaths(int n, int m) {
+        vector<int> prev(n,0);
+        // dp[0][0] = 1;
+        for(int i = 0;i <n;i++)
+        {
+            vector<int> temp(m, 0);
+            for(int j = 0;j <m;j++)
+            {
+                if(i == 0 && j == 0)
+                {
+                    temp[j] = 1;
+                    continue;
+                }
+                int up = 0, left = 0;
+                if(i > 0)
+                    up = prev[j];
+                if(j > 0)
+                    left = temp[j - 1];
+                temp[j] = up + left;
+            }
+            prev = temp;
+        }
+        return prev[m - 1];
+    }
+};
