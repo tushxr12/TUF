99acda274442d33ad4c86c673db7ede3a82adc67
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-17 15:03:50 +0530

    Add space optmized approach

--- Dynamic Programming/DP on grids/Unique paths II.cpp
+++ Dynamic Programming/DP on grids/Unique paths II.cpp
@@ -87,3 +87,41 @@ public:
     }
 };
 
+//Space optimization
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        if(matrix[0][0] == 1 || matrix[n - 1][m - 1] == 1)
+            return 0;
+        
+        vector<int> prev(n,-1);
+        for(int i = 0;i < n;i++)
+        {
+            vector<int> temp(m, -1);
+            for(int j = 0;j < m;j++)
+            {
+                if(i == 0 && j == 0 && temp[j] != 1)
+                {
+                    temp[j] = 1;
+                    continue;
+                }
+
+                int up = 0, left = 0;
+                if(i > 0 && matrix[i - 1][j] != 1)
+                {
+                    up = prev[j];
+                }
+
+                if(j > 0 && matrix[i][j - 1] != 1)
+                {
+                    left = temp[j - 1];
+                }
+                temp[j] = up + left;
+            }
+            prev = temp;
+        }
+        return prev[m-1];
+    }
+};
