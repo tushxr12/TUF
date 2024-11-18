cf812d5e328294f9fdb81d1e3839868485daa924
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-18 14:26:08 +0530

    Add memoization approach

--- Dynamic Programming/DP on grids/Minimum path sum in grid.cpp
+++ Dynamic Programming/DP on grids/Minimum path sum in grid.cpp
@@ -1,3 +1,4 @@
+//Recursion
 class Solution {
 private:
     int f(int i,int j, vector<vector<int>> &matrix)
@@ -21,6 +22,39 @@ public:
         for(int j = 0;j < m;j++)
         {
             int ans = f(n - 1,j,matrix);
+            mini = min(mini, ans);
+        }
+        return mini;
+    }
+};
+
+//Memoization
+class Solution {
+private:
+    int f(int i,int j, vector<vector<int>> &matrix,vector<vector<int>> &dp)
+    {
+        if(j < 0 || j >= matrix[0].size())
+            return 1e9;
+        if(i == 0)
+            return matrix[0][j];
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int up = matrix[i][j] + f(i - 1,j,matrix,dp);
+        int leftDiagonal = matrix[i][j] + f(i - 1,j - 1,matrix,dp);
+        int rightDiagonal = matrix[i][j] + f(i - 1,j + 1,matrix,dp);
+
+        return dp[i][j] = min({up, leftDiagonal, rightDiagonal});
+    }
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int mini =INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        for(int j = 0;j < m;j++)
+        {
+            int ans = f(n - 1,j,matrix,dp);
             mini = min(mini, ans);
         }
         return mini;
