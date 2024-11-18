0aa48fb2f283fde53f748f3c78dad65f4f57f6ba
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-18 14:58:31 +0530

    Add memoization approach

--- Dynamic Programming/DP on grids/Triangle.cpp
+++ Dynamic Programming/DP on grids/Triangle.cpp
@@ -20,3 +20,30 @@ public:
         return f(0,0,triangle);
     }
 };
+
+//Memoization
+class Solution {
+private:
+    int f(int i,int j,int n,vector<vector<int>> &triangle,vector<vector<int>> &dp)
+    {
+        if(dp[i][j] != -1)
+            return dp[i][j];  
+
+        if(i == n - 1)
+        {
+            return triangle[i][j];
+        }
+          
+
+        int down = triangle[i][j] + f(i + 1,j,n,triangle,dp);
+        int rightDiagonal = triangle[i][j] + f(i + 1,j + 1,n,triangle,dp);
+
+        return dp[i][j] = min(down, rightDiagonal);
+    }
+public:
+    int minTriangleSum(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return f(0,0,n,triangle,dp);
+    }
+};
