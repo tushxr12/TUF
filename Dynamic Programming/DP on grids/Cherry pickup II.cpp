b4c65b91f94df62265dfb59964f2ae34e0d6fd2c
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-19 22:29:17 +0530

    Add memoization approach

--- Dynamic Programming/DP on grids/Cherry pickup II.cpp
+++ Dynamic Programming/DP on grids/Cherry pickup II.cpp
@@ -38,3 +38,46 @@ public:
         return f(0,0,c - 1,r,c,matrix);
     }
 };
+
+//Memoization
+class Solution {
+private:
+    int f(int i,int j1,int j2,int r,int c, vector<vector<int>> &matrix,vector<vector<vector<int>>> &dp)
+    {
+        if(j1 < 0 || j2 <= 0|| j1 >= c || j2 >= c)
+        {
+            return -1e8;
+        }
+        if(dp[i][j1][j2] != -1)
+            return dp[i][j1][j2];
+        if(i == r -1)
+        {
+            if(j1 == j2)    
+                return matrix[i][j1];
+            else
+                return matrix[i][j1] + matrix[i][j2];
+        }
+
+        int maxi = -1e8;
+        for(int dj1 = -1; dj1 <= +1;dj1++)
+        {
+            for(int dj2 = -1;dj2 <= +1;dj2++)
+            {
+                int val = 0;
+                if(j1 == j2)
+                    val = matrix[i][j1];
+                else
+                    val = matrix[i][j1] + matrix[i][j2];
+                val += f(i + 1,j1+dj1,j2+dj2,r,c,matrix,dp);
+                maxi = max(maxi, val);
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+public:
+    int cherryPickup(vector<vector<int>>& matrix) {
+        int r = matrix.size(), c = matrix[0].size();
+        vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c,-1)));
+        return f(0,0,c - 1,r,c,matrix,dp);
+    }
+};
