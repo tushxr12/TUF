97f705c97836575f4404d50983e851a06cb5724b
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-20 23:39:19 +0530

    Add memoization approach

--- Dynamic Programming/DP on stocks/Best time to buy and sell stock II.cpp
+++ Dynamic Programming/DP on stocks/Best time to buy and sell stock II.cpp
@@ -24,3 +24,34 @@ public:
     }   
 };
 
+//Memoization
+class Solution{
+private:
+    int f(int ind,int buy,vector<int> &arr, vector<vector<int>> &dp)
+    {
+        if(ind == arr.size())
+        {
+            return 0;
+        }
+
+        if(dp[ind][buy] != -1)  
+            return dp[ind][buy];
+        
+        long profit = 0;
+        if(buy)
+        {
+            profit = max(-arr[ind] + f(ind + 1,0,arr,dp), 0 + f(ind + 1,1,arr,dp));
+        }
+        else
+        {
+            profit = max(arr[ind] + f(ind + 1,1,arr,dp), 0 + f(ind + 1,0,arr,dp));
+        }
+        return dp[ind][buy] = profit;
+    }
+public:
+    int stockBuySell(vector<int> arr, int n){
+        vector<vector<int>> dp(n, vector<int> (2,-1));
+        return f(0,1,arr,dp);
+    }   
+};
+
