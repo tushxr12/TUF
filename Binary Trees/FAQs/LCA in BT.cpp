f7c31d5b3175a526a16adcbff3965518d23fab99
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-22 23:14:44 +0530

    Remove description

--- Binary Trees/FAQs/LCA in BT.cpp
+++ Binary Trees/FAQs/LCA in BT.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int data;
- *     TreeNode *left;
- *     TreeNode *right;
- *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
- * };
- **/
-
 class Solution
 {
 public:
