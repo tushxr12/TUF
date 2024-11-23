6eaf4bfdb470c68b4961312502ee72d4f37b0283
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-23 10:40:15 +0530

    Add tabulation approach

--- Dynamic Programming/DP on stocks/Best time to buy and sell stock III.cpp
+++ Dynamic Programming/DP on stocks/Best time to buy and sell stock III.cpp
@@ -42,3 +42,30 @@ public:
         return f(0,1,2,arr,dp);
     }
 };
+
+//Tabulation
+class Solution{
+public:
+    int stockBuySell(vector<int> arr, int n){
+        vector<vector<vector<int>>> dp(n + 1,vector<vector<int>> (2, vector<int>(3,0)));
+
+        for(int ind = n - 1; ind >= 0; ind--)
+        {
+            for(int buy = 0; buy <= 1;buy++)
+            {
+                for(int cap = 1; cap <= 2;cap++)
+                {
+                    if(buy == 0)
+                    {
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][0][cap], -arr[ind] + dp[ind + 1][1][cap]);
+                    }
+                    if(buy == 1)
+                    {
+                        dp[ind][buy][cap] = max(0 + dp[ind + 1][1][cap], arr[ind] + dp[ind + 1][0][cap - 1]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
+    }
+};
