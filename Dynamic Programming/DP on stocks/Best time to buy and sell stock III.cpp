768a7e7b1568bb5f60fce848ac32417168f90f6d
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-23 10:42:57 +0530

    Add space optimization approach

--- Dynamic Programming/DP on stocks/Best time to buy and sell stock III.cpp
+++ Dynamic Programming/DP on stocks/Best time to buy and sell stock III.cpp
@@ -69,3 +69,32 @@ public:
         return dp[0][0][2];
     }
 };
+
+//Space Optimization
+class Solution{
+public:
+    int stockBuySell(vector<int> arr, int n){
+        vector<vector<int>> front(2,vector<int>(3,0));
+        vector<vector<int>> curr(2, vector<int>(3,0));
+
+        for(int ind = n - 1; ind >= 0; ind--)
+        {
+            for(int buy = 0; buy <= 1;buy++)
+            {
+                for(int cap = 1; cap <= 2;cap++)
+                {
+                    if(buy == 0)
+                    {
+                        curr[buy][cap] = max(0 + front[0][cap], -arr[ind] + front[1][cap]);
+                    }
+                    if(buy == 1)
+                    {
+                        curr[buy][cap] = max(0 + front[1][cap], arr[ind] + front[0][cap - 1]);
+                    }
+                }
+            }
+            front = curr;
+        }
+        return front[0][2];
+    }
+};
