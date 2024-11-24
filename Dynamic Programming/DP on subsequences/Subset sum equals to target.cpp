821217277e2f2c9f85a69829e510da426fc16833
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-24 15:27:39 +0530

    Add tabulation approach

--- Dynamic Programming/DP on subsequences/Subset sum equals to target.cpp
+++ Dynamic Programming/DP on subsequences/Subset sum equals to target.cpp
@@ -55,3 +55,34 @@ public:
         return f(n - 1,arr,target, dp);
     }
 };
+
+//Tabulation
+class Solution{  
+public:
+    bool isSubsetSum(vector<int>arr, int t){
+        int n = arr.size();
+        vector<vector<bool>> dp(n, vector<bool>(t + 1,0));
+
+        for(int ind = 0; ind < n;ind++)
+        {
+            dp[ind][0] = true;
+        }
+
+        dp[0][arr[0]] = true;
+
+        for(int ind = 1; ind < n;ind++)
+        {
+            for(int target = 1; target <= t;target++)
+            {
+                bool notTake = dp[ind - 1][target];
+                bool take = false;
+                if(target >= arr[ind]){
+                    take = dp[ind - 1][target - arr[ind]];
+                }
+                dp[ind][target] = (take || notTake);
+            }
+        }
+
+        return dp[n-1][t];
+    }
+};
