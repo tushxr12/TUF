036f38e2bb167000b770ca56314203e5cb8f4464
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-24 15:45:08 +0530

    Add memoization approach

--- Dynamic Programming/DP on subsequences/Partition equal subset sum.cpp
+++ Dynamic Programming/DP on subsequences/Partition equal subset sum.cpp
@@ -25,3 +25,40 @@ public:
         return f(n - 1, totalSum/2, arr);
     }
 };
+
+//Memoization
+class Solution{
+private:
+    bool f(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
+    {
+        if(target == 0)
+            return true;
+
+        if(ind == 0)
+            return (arr[ind] == target);
+        
+        if(dp[ind][target] != -1)
+            return dp[ind][target];
+        
+        bool notPick = f(ind - 1, target, arr, dp);
+
+        bool pick = false;
+        if(target >= arr[ind])
+            pick = f(ind - 1, target - arr[ind], arr, dp);
+
+        return dp[ind][target] = (pick || notPick);
+    }
+public:
+    bool equalPartition(int n, vector<int> arr) {
+        int totalSum = 0;
+        for(auto i : arr)
+            totalSum += i;
+        
+        if(totalSum % 2)
+            return false;
+
+        vector<vector<int>> dp(n, vector<int>(totalSum/2 + 1, -1));
+        
+        return f(n - 1, totalSum/2, arr, dp);
+    }
+};
