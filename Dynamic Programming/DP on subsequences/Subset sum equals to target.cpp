88caa861f256752f861b572c4dd708f7293ef7f8
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-24 15:22:44 +0530

    Add memoization approach

--- Dynamic Programming/DP on subsequences/Subset sum equals to target.cpp
+++ Dynamic Programming/DP on subsequences/Subset sum equals to target.cpp
@@ -24,3 +24,34 @@ public:
         return f(n - 1,arr,target);
     }
 };
+
+//Memoization
+class Solution{   
+private:
+    bool f(int ind, vector<int> &arr, int target, vector<vector<int>> &dp)
+    {
+        if(target == 0){
+            return true;
+        }
+
+        if(ind == 0){
+            return (arr[0] == target);
+        }
+
+        if(dp[ind][target] != -1)
+            return dp[ind][target];
+
+        bool notTake = f(ind - 1, arr, target, dp);
+        bool take = false;
+        if(target >= arr[ind]){
+            take = f(ind - 1, arr, target - arr[ind], dp);
+        }
+        return dp[ind][target] = (take || notTake);
+    }
+public:
+    bool isSubsetSum(vector<int>arr, int target){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1,-1));
+        return f(n - 1,arr,target, dp);
+    }
+};
