73e8bb6611fb9f4350b161773321f5aab0f1c300
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-25 23:54:13 +0530

    Add tabulation approach

--- Dynamic Programming/DP on subsequences/Count subsets with sum K.cpp
+++ Dynamic Programming/DP on subsequences/Count subsets with sum K.cpp
@@ -45,3 +45,32 @@ class Solution{
 		return f(arr.size() - 1,K,arr, dp);
 	}
 };
+
+//Tabulation
+class Solution{
+	int modulo = 1e9+7;
+	public:
+	int perfectSum(vector<int>&arr, int K){
+		vector<vector<int>> dp(arr.size(), vector<int> (K + 1, 0));
+		for(int ind = 0; ind < arr.size();ind++)
+		{
+			dp[ind][0] = 1;
+		}
+
+		if(K >= arr[0])
+			dp[0][arr[0]] = 1;
+
+		for(int ind = 1;ind < arr.size();ind++)
+		{
+			for(int k = 1; k <= K;k++)
+			{
+				int notPick = dp[ind - 1][k];
+				int pick = 0;
+				if(k >= arr[ind])
+					pick = dp[ind - 1][k - arr[ind]];
+				dp[ind][k] = (pick + notPick)%modulo;
+			}
+		}
+		return dp[arr.size() - 1][K];
+	}
+};
