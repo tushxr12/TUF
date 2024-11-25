bdc871995900817231b51b13bf02f022e8cfb4fb
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-25 23:48:32 +0530

    Add memoization appraoch

--- Dynamic Programming/DP on subsequences/Count subsets with sum K.cpp
+++ Dynamic Programming/DP on subsequences/Count subsets with sum K.cpp
@@ -1,3 +1,4 @@
+//Recursion
 class Solution{
 	int modulo = 1e9+7;
 	private:
@@ -17,5 +18,30 @@ class Solution{
 	public:
 	int perfectSum(vector<int>&arr, int K){
 		return f(arr.size() - 1,K,arr);
+	}
+};
+
+//Memoization
+class Solution{
+	int modulo = 1e9+7;
+	private:
+	int f(int ind,int k, vector<int> &arr, vector<vector<int>> &dp)
+	{
+		if(k == 0)
+			return 1;
+		if(ind == 0)
+			return (arr[ind] == k);
+		if(dp[ind][k] != -1)
+			return dp[ind][k];
+		int notPick = f(ind - 1,k,arr,dp);
+		int pick = 0;
+		if(k >= arr[ind])
+			pick = f(ind - 1,k - arr[ind],arr,dp);
+		return (pick + notPick)%modulo;
+	}
+	public:
+	int perfectSum(vector<int>&arr, int K){
+		vector<vector<int>> dp(arr.size(), vector<int> (K + 1, -1));
+		return f(arr.size() - 1,K,arr, dp);
 	}
 };
