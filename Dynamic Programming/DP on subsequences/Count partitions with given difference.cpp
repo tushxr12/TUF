c91604fc2174fd5772f995cd42eff398f4865e79
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-26 16:46:17 +0530

    Add memoization approach

--- Dynamic Programming/DP on subsequences/Count partitions with given difference.cpp
+++ Dynamic Programming/DP on subsequences/Count partitions with given difference.cpp
@@ -33,3 +33,44 @@ class Solution {
         return f(n -1, (totalSum - diff)/2, arr);
     }
 };
+
+//Memoization
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  int modulo = 1e9 + 7;
+  private:
+  int f(int ind,int k,vector<int> &arr, vector<vector<int>> &dp)
+  {
+      if(k == 0)
+        return 1;
+      
+      if(ind == 0)
+      {
+        return arr[0] == k;
+      }
+
+      if(dp[ind][k] != -1)
+        return dp[ind][k];
+
+      int notPick = f(ind - 1,k,arr, dp);
+
+      int pick = 0;
+      if(k >= arr[ind])
+        pick = f(ind - 1,k - arr[ind], arr, dp);
+      
+      return dp[ind][k] =  (pick + notPick)%modulo;
+  }
+  public:
+  int countPartitions(int n, int diff, vector<int>& arr) {
+        int totalSum = 0;
+        for(auto i : arr){
+          totalSum += i;
+        }
+
+        int reqSum = (totalSum - diff)/2;
+        vector<vector<int>> dp(n, vector<int>(reqSum + 1,-1));
+        return f(n -1, reqSum, arr, dp);
+    }
+};
