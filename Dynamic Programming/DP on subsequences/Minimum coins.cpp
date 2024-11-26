93071104c483c93ba5d0a3f518ff18e38977a291
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-26 17:52:41 +0530

    Practice DP problem | Add recursion approach

---
+++ Dynamic Programming/DP on subsequences/Minimum coins.cpp
@@ -0,0 +1,28 @@
+//Recursion
+class Solution{
+	private:
+	int f(int ind, int amount, vector<int> &coins)
+	{	
+		if(ind == 0)
+		{
+			if(amount % coins[0] == 0)
+			{
+				return (amount/coins[0]);
+			}else
+			{
+				return -1;
+			}
+		}
+
+		int notPick = f(ind - 1, amount, coins);
+		int pick = INT_MAX;
+		if(amount >= coins[ind]){
+			pick = 1 + f(ind, amount - coins[ind], coins);
+		}
+		return min(pick, notPick);
+	}
+	public:
+	int MinimumCoins(vector<int>& coins, int amount) {
+		return f(coins.size() - 1, amount, coins);
+    }
+};
