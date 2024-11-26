c15daf46c9ebe258169d1e062b3a9d972d0b1ec7
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-26 17:33:55 +0530

    Add tabulation approach

--- Dynamic Programming/DP on subsequences/0 and 1 Knapsack.cpp
+++ Dynamic Programming/DP on subsequences/0 and 1 Knapsack.cpp
@@ -59,3 +59,35 @@ class Solution
         return f(n - 1,W,wt,val,dp);
     }
 };
+
+//Tabulation
+class Solution
+{
+    public:
+    int knapsack01(vector<int>& wt, vector<int>& val, int n, int W) {
+        vector<vector<int>> dp(n, vector<int>(W + 1,0));
+
+        for(int i = wt[0]; i<=W;i++)
+        {
+            dp[0][i] = val[0];
+        }
+
+        for(int ind = 1;ind < n;ind++)
+        {
+            for(int w=0; w<=W;w++)
+            {
+                int notTake = dp[ind -1][w];
+                int take = INT_MIN;
+
+                if(wt[ind] <= w)
+                {
+                    take = val[ind] + dp[ind - 1][w - wt[ind]];
+                }
+
+                dp[ind][w] = max(take, notTake);
+            }
+        }
+
+        return dp[n - 1][W];
+    }
+};
