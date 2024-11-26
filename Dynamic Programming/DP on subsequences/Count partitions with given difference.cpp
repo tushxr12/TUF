0f1fb780d37d69a132e5b53a97b20874c741aad2
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-26 16:57:18 +0530

    Add tabulation approach

--- Dynamic Programming/DP on subsequences/Count partitions with given difference.cpp
+++ Dynamic Programming/DP on subsequences/Count partitions with given difference.cpp
@@ -74,3 +74,43 @@ class Solution {
         return f(n -1, reqSum, arr, dp);
     }
 };
+
+//Tabulation
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  int modulo = 1e9 + 7;
+  public:
+  int countPartitions(int n, int diff, vector<int>& arr) {
+        int totalSum = 0;
+        for(auto i : arr){
+          totalSum += i;
+        }
+        if(totalSum - diff < 0 || (totalSum - diff) %2){
+          return 0;
+        }
+        int reqSum = (totalSum - diff)/2;
+        vector<vector<int>> dp(n, vector<int>(reqSum + 1,0));
+        for(int ind = 0;ind < n;ind++)
+          dp[ind][0] = 1;
+
+        if(reqSum >= arr[0])
+          dp[0][arr[0]] = 1;
+
+        for(int ind = 1; ind < n;ind++)
+        {
+          for(int k = 1;k <= reqSum;k++)
+          {
+              int notPick = dp[ind - 1][k];
+
+              int pick = 0;
+              if(k >= arr[ind])
+                pick = dp[ind - 1][k - arr[ind]];
+              
+              dp[ind][k] =  (pick + notPick)%modulo;
+          }
+        }
+        return dp[n -1][reqSum];
+    }
+};
