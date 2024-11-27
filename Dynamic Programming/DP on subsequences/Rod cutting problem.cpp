c5515209b77bc1722d2a4a389a5cd26f3e55229e
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-11-27 14:47:43 +0530

    Add memoization approach

--- Dynamic Programming/DP on subsequences/Rod cutting problem.cpp
+++ Dynamic Programming/DP on subsequences/Rod cutting problem.cpp
@@ -21,3 +21,31 @@ class Solution{
         return f(n - 1,n,price);
     }
 };
+
+//Memoization
+class Solution{
+  private:
+    int f(int ind,int n, vector<int> &prices, vector<vector<int>> &dp)
+    {
+        if(ind == 0)
+        {
+            return n*prices[0];
+        }
+
+        if(dp[ind][n] != -1)  
+          return dp[ind][n];
+
+        int notTake = f(ind - 1,n,prices,dp);
+        int take = INT_MIN;
+        int rodLength = ind + 1;
+        if(rodLength <= n){
+          take = prices[ind] + f(ind,n - rodLength,prices,dp);
+        }
+        return dp[ind][n] = max(take, notTake);
+    }
+  public:
+    int rodCutting(vector<int> price, int n) {
+      vector<vector<int>> dp(n, vector<int>(n + 1,-1));
+      return f(n - 1,n,price,dp);
+    }
+};
