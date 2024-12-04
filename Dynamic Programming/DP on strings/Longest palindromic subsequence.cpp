ee8323f6a075c1874f26fc1e823c0add14e81a41
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-04 23:38:48 +0530

    Add tabulation approach

--- Dynamic Programming/DP on strings/Longest palindromic subsequence.cpp
+++ Dynamic Programming/DP on strings/Longest palindromic subsequence.cpp
@@ -29,3 +29,41 @@ class Solution{
         return lcs(t,s);
     }
 };
+
+//Tabulation
+class Solution{
+  private:
+    int lcs(string s1, string s2) {
+      int n1 = s1.size(), n2 = s2.size();
+      vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+
+      for(int ind1 = 0; ind1 <= n1;ind1++){
+        dp[ind1][0] = 0;
+      }
+
+      for(int ind2 = 0;ind2 <= n2;ind2++){
+        dp[0][ind2] = 0;
+      }
+
+      for(int ind1 = 1;ind1 <= n1;ind1++)
+      {
+        for(int ind2 = 1;ind2 <= n2;ind2++)
+        {
+          if(s1[ind1 - 1] == s2[ind2 - 1])
+          {
+            dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+          }
+          else
+            dp[ind1][ind2] = max(dp[ind1 -1][ind2], dp[ind1][ind2 - 1]);
+        }
+      }
+
+      return dp[n1][n2];
+    }
+  public:
+    int longestPalinSubseq(string s) {
+        string t = s;
+        reverse(s.begin(), s.end());
+        return lcs(t,s);
+    }
+};
