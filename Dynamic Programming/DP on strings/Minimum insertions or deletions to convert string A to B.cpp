6eacdea8c65cee04ffcd309e236c0bec1e6e3f08
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-10 19:02:53 +0530

    Practice DP problem | Add recursion approach

---
+++ Dynamic Programming/DP on strings/Minimum insertions or deletions to convert string A to B.cpp
@@ -0,0 +1,27 @@
+//Recursion
+class Solution{
+	private:
+	int f(int ind1, int ind2, string s1, string s2)
+	{
+		if(ind1 < 0 || ind2 < 0)
+		return 0;
+
+		//If match
+		if(s1[ind1] == s2[ind2])
+		{
+		return 1 + f(ind1 - 1,ind2 - 1, s1, s2);
+		}
+
+		//If Not match
+		return max(f(ind1 -1,ind2,s1 ,s2) , f(ind1, ind2 - 1, s1, s2));
+	}
+	int lcs(string str1, string str2) {
+		return f(str1.size() - 1, str2.size() - 1, str1, str2);
+	}
+	public:
+	int minOperations(string str1, string str2) { 
+	    int n = str1.size() , m = str2.size();
+		int lcss = lcs(str1, str2);
+		return (n + m - 2*lcss);
+	} 
+};
