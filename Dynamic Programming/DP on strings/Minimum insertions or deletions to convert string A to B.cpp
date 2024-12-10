d376fb26578eba9bbfd4a86fbcb8c2d6fdcff515
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-10 19:04:16 +0530

    Add memoization approach

--- Dynamic Programming/DP on strings/Minimum insertions or deletions to convert string A to B.cpp
+++ Dynamic Programming/DP on strings/Minimum insertions or deletions to convert string A to B.cpp
@@ -25,3 +25,36 @@ class Solution{
 		return (n + m - 2*lcss);
 	} 
 };
+
+//Memoization
+class Solution{
+	private:
+	int f(int ind1, int ind2, string s1, string s2, vector<vector<int>> &dp)
+	{
+		if(ind1 < 0 || ind2 < 0)
+		return 0;
+
+		if(dp[ind1][ind2] != -1)
+		return dp[ind1][ind2];
+
+		//If match
+		if(s1[ind1] == s2[ind2])
+		{
+		return 1 + f(ind1 - 1,ind2 - 1, s1, s2, dp);
+		}
+
+		//If Not match
+		return dp[ind1][ind2] = max(f(ind1 -1,ind2,s1 ,s2, dp) , f(ind1, ind2 - 1, s1, s2, dp));
+	}
+    int lcs(string str1, string str2) {
+		int n1 = str1.size(), n2 = str2.size();
+		vector<vector<int>> dp(n1, vector<int>(n2, -1));
+		return f(str1.size() - 1, str2.size() - 1, str1, str2, dp);
+    }
+	public:
+	int minOperations(string str1, string str2) { 
+	    int n = str1.size() , m = str2.size();
+		int lcss = lcs(str1, str2);
+		return (n + m - 2*lcss);
+	} 
+};
