f865a06d7ab6344b97ff0fee8ae4436069577b94
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-13 19:11:10 +0530

    Add tabulation approach

--- Dynamic Programming/DP on strings/Distinct subsequences.cpp
+++ Dynamic Programming/DP on strings/Distinct subsequences.cpp
@@ -46,3 +46,36 @@ class Solution{
 		return f(n - 1,m - 1,s,t,dp);
 	}
 };
+
+//Tabulation
+class Solution{
+  int modulo = 1e9 + 7;
+  public:	
+	int distinctSubsequences(string s, string t){
+		int n = s.size(), m = t.size();
+		vector<vector<int>> dp(n + 1, vector<int>(m + 1,0));
+		
+		for(int i =0;i <= n;i++)
+			dp[i][0] = 1;
+		
+		for(int j = 1;j <= m;j++)
+			dp[0][j] = 0;
+
+		for(int i = 1;i <= n;i++)
+		{
+			for(int j = 1;j <= m;j++)
+			{
+				if(s[i - 1] == t[j - 1])
+				{
+					dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j])%modulo;
+				}
+				else
+				{
+					dp[i][j] = dp[i - 1][j];
+				}
+			}
+		}
+
+		return dp[n][m];
+	}
+};
