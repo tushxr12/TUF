ca96882dffce62fdc10319f260b9f7cfe1935e25
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-13 19:14:08 +0530

    Add space optimization

--- Dynamic Programming/DP on strings/Distinct subsequences.cpp
+++ Dynamic Programming/DP on strings/Distinct subsequences.cpp
@@ -79,3 +79,33 @@ class Solution{
 		return dp[n][m];
 	}
 };
+
+//Space optimization
+class Solution{
+  int modulo = 1e9 + 7;
+  public:	
+	int distinctSubsequences(string s, string t){
+		int n = s.size(), m = t.size();
+		vector<int> prev(m + 1,0), curr(m + 1,0);
+		
+		prev[0] = curr[0] = 1;
+
+		for(int i = 1;i <= n;i++)
+		{
+			for(int j = 1;j <= m;j++)
+			{
+				if(s[i - 1] == t[j - 1])
+				{
+					curr[j] = (prev[j - 1] + prev[j])%modulo;
+				}
+				else
+				{
+					curr[j] = prev[j];
+				}
+			}
+			prev = curr;
+		}
+
+		return prev[m];
+	}
+};
