16ac80dac43fe916ca262b6d83922b58d805b2da
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-14 16:38:02 +0530

    Add tabulation approach

--- Dynamic Programming/DP on strings/Edit distance.cpp
+++ Dynamic Programming/DP on strings/Edit distance.cpp
@@ -47,3 +47,29 @@ class Solution {
       return f(n - 1,m-1,s1,s2,dp);
     }
 };
+
+//Tabulation
+class Solution {
+  public:
+    int editDistance(string s1, string s2) {
+      int n = s1.size(), m = s2.size();
+      vector<vector<int>> dp(n + 1, vector<int>(m + 1,0));
+
+      for(int i = 0;i <= n;i++)
+        dp[i][0] = i;
+      for(int j = 0;j <= m;j++)
+        dp[0][j] = j;
+
+      for(int i = 1;i <= n;i++)
+      {
+        for(int j = 1;j <= m;j++)
+        {
+          if(s1[i - 1] == s2[j - 1])
+            dp[i][j] = dp[i - 1][j - 1];
+          else
+            dp[i][j] = 1 +  min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]});
+        }
+      }
+      return dp[n][m];
+    }
+};
