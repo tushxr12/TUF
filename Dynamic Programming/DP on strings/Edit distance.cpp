feb14172039e2fbf9b103e666ef4ac5191b3cb5c
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-14 16:41:24 +0530

    Add space optimization

--- Dynamic Programming/DP on strings/Edit distance.cpp
+++ Dynamic Programming/DP on strings/Edit distance.cpp
@@ -73,3 +73,29 @@ class Solution {
       return dp[n][m];
     }
 };
+
+//Space optimization
+class Solution {
+  public:
+    int editDistance(string s1, string s2) {
+      int n = s1.size(), m = s2.size();
+      vector<int> curr(m + 1,0), prev(m + 1,0);
+      
+      for(int j = 0;j <= m;j++)
+        prev[j] = j;
+
+      for(int i = 1;i <= n;i++)
+      {
+        curr[0] = i;
+        for(int j = 1;j <= m;j++)
+        {
+          if(s1[i - 1] == s2[j - 1])
+            curr[j] = prev[j - 1];
+          else
+            curr[j] = 1 +  min({curr[j - 1], prev[j], prev[j - 1]});
+        }
+        prev = curr;
+      }
+      return prev[m];
+    }
+};
