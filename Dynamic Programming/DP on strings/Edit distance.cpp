2bb14567a461fcad24548c6cc6e370e632cef851
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-14 16:31:33 +0530

    Add memoization approach

--- Dynamic Programming/DP on strings/Edit distance.cpp
+++ Dynamic Programming/DP on strings/Edit distance.cpp
@@ -20,3 +20,30 @@ class Solution {
       return f(n - 1,m-1,s1,s2);
     }
 };
+
+//Memoization
+class Solution {
+  private:
+    int f(int i,int j, string &s1, string &s2,vector<vector<int>> &dp)
+    {
+      if(i < 0)
+        return j + 1;
+
+      if(j < 0)
+        return i + 1;
+
+      if(dp[i][j] != -1)
+        return dp[i][j];
+
+      if(s1[i] == s2[j])
+        return dp[i][j] = f(i - 1,j - 1,s1,s2,dp);
+      
+      return dp[i][j] = 1 +  min({f(i,j - 1,s2,s2,dp), f(i - 1,j,s1,s2,dp), f(i - 1,j - 1,s1,s2,dp)});
+    }
+  public:
+    int editDistance(string s1, string s2) {
+      int n = s1.size(), m = s2.size();
+      vector<vector<int>> dp(n, vector<int>(m,-1));
+      return f(n - 1,m-1,s1,s2,dp);
+    }
+};
