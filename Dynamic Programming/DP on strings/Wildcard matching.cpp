30639bb474c43232739a5c21048620fc70f82dac
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-14 17:15:07 +0530

    Add tabulation approach

--- Dynamic Programming/DP on strings/Wildcard matching.cpp
+++ Dynamic Programming/DP on strings/Wildcard matching.cpp
@@ -59,3 +59,43 @@ class Solution {
     return f(n-1, m-1, str, pat,dp);
   }
 };
+
+//Tabulation
+class Solution {
+  public:
+  bool wildCard(string s1, string s2) {
+    int n = s1.size(), m = s2.size();
+    vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+    dp[0][0] = true;
+    for(int j = 1;j <= m;j++)
+      dp[0][j] = false;
+    
+    for(int i = 1;i <= n;i++)
+    {
+      bool fl = true;
+      for(int ii = 1; ii <= i;ii++)
+      {
+        if(s1[ii-1] != '*')
+        {
+          fl = false;
+          break;
+        }
+      }
+      dp[i][0] = fl;
+    }
+
+    for(int i = 1;i <= n;i++)
+    {
+      for(int j = 1;j <= m;j++)
+      {
+          if(s1[i - 1] == s2[j - 1] || s1[i - 1] == '?')
+            dp[i][j] = dp[i - 1][j - 1];
+          else if(s1[i - 1] == '*')
+            dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+          else
+            dp[i][j] = false;
+      }
+    }
+    return dp[n][m];
+  }
+};
