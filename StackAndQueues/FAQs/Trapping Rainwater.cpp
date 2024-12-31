acca7e516a8c10531ab8868a56b95027c1685f2c
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-01 00:59:22 +0530

    Add optimal approach

--- StackAndQueues/FAQs/Trapping Rainwater.cpp
+++ StackAndQueues/FAQs/Trapping Rainwater.cpp
@@ -24,3 +24,41 @@ public:
         return ans;
     }
 };
+
+//Optimzal
+class Solution
+{
+public:
+    int trap(vector<int> &height){
+        int lMax = 0, rMax = 0, total = 0, left = 0 ,right = height.size() - 1;
+
+        while(left < right)
+        {
+            if(height[left] <= height[right])
+            {
+                if(lMax > height[left])
+                {
+                    total += (lMax - height[left]);
+                }
+                else
+                {
+                    lMax = height[left];
+                }
+                left += 1;
+            }
+            else
+            {
+                if(rMax > height[right])
+                {
+                    total += (rMax - height[right]);
+                }
+                else
+                {
+                    rMax = height[right];
+                }
+                right -= 1;
+            }
+        }
+        return total;
+    }
+};
