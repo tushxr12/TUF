cb9d9cbe51005c24fb67678df725917f76988e6a
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-01 14:26:34 +0530

    Practice stack problem

---
+++ StackAndQueues/FAQs/Maximum Rectangles.cpp
@@ -0,0 +1,68 @@
+class Solution
+{
+private:
+    int largestRectangleArea(vector<int> &heights) {
+        int n = heights.size();
+        int nse ,pse , area, maxArea = 0;
+        stack<int> st;
+
+        for(int i = 0;i < n;i++)
+        {
+            while(!st.empty() && heights[st.top()] >= heights[i])
+            {
+                int ind = st.top();
+                st.pop();
+
+                nse = i;
+                pse = st.empty() ? -1 : st.top();
+                area = heights[i] * (nse - pse - 1);
+                maxArea = max(maxArea, area);
+            }
+
+            st.push(i);
+        }
+
+        while(!st.empty())
+        {
+            int ind = st.top();
+            st.pop();
+
+            nse = n;
+            pse = st.empty() ? -1 : st.top();
+            area = heights[ind] * (nse - pse - 1);
+            maxArea = max(maxArea, area);
+        }
+        return maxArea;
+    }
+public:
+    int maximalAreaOfSubMatrixOfAll1(vector<vector<int>> &matrix){
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<vector<int>> prefSum(n, vector<int>(m));
+
+        for(int j = 0;j  <m;j++)
+        {
+            int sum = 0;
+            for(int i = 0;i < n;i++)
+            {
+                sum += matrix[i][j];
+
+                if(matrix[i][j] == 0)
+                {
+                    prefSum[i][j] = 0;
+                    sum = 0;
+                }
+                prefSum[i][j] = sum;
+            }
+        }
+
+        int maxArea = 0;
+        for(int i = 0;i < n;i++)
+        {
+            int area = largestRectangleArea(prefSum[i]);
+            maxArea = max(maxArea, area);
+        }
+        return maxArea;
+    }
+};
