305eae7b21ea1f3a0a9ed76cbfa9bd11908d6210
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-01 13:43:53 +0530

    Add optimal approach

--- StackAndQueues/FAQs/Largest rectangle in a histogram.cpp
+++ StackAndQueues/FAQs/Largest rectangle in a histogram.cpp
@@ -63,3 +63,45 @@ public:
         return largestArea;
     }
 };
+
+//Optimal
+class Solution
+{
+public:
+    int largestRectangleArea(vector<int> &heights) {
+        int n = heights.size();
+        stack<int> st;
+        int largestArea = 0;
+        int area;
+        int nse , pse;
+
+        for(int i = 0;i < n;i++)
+        {
+            while(!st.empty() && heights[st.top()] >= heights[i])
+            {
+                int ind = st.top();
+                st.pop();
+
+                pse = st.empty() ? -1 : st.top();
+
+                nse = i;
+
+                area = heights[ind] * (nse - pse - 1);
+                largestArea = max(largestArea, area);
+            }
+            st.push(i);
+        }
+
+        while(!st.empty())
+        {
+            nse = n;
+            int ind = st.top();
+            st.pop();
+
+            pse = st.empty() ? - 1 : st.top();
+            area = heights[ind] * (nse - pse - 1);
+            largestArea = max(largestArea, area);
+        }
+        return largestArea;
+    }
+};
