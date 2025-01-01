da1a4a5d3107d072a62abf98aab1516dc4dc1fee
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-01 13:21:37 +0530

    Practice stack problem | Add bruteforce approach

---
+++ StackAndQueues/FAQs/Largest rectangle in a histogram.cpp
@@ -0,0 +1,65 @@
+class Solution
+{
+private:
+    vector<int> findNSE(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+
+        for(int i = n - 1; i >= 0;i--)
+        {
+            int currElement= arr[i];
+ 
+            while(!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            
+            ans[i] = st.empty() ? n : st.top();
+
+            st.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> findPSE(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+
+        for(int i = 0; i < n;i++)
+        {
+            int currElement= arr[i];
+ 
+            while(!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            
+            ans[i] = st.empty() ? -1 : st.top();
+
+            st.push(i);
+        }
+        return ans;
+    }
+public:
+    int largestRectangleArea(vector<int> &heights) {
+        vector<int> nse = findNSE(heights);
+        vector<int> pse = findPSE(heights);
+
+        // cout<<"NSE : ";
+        // for(auto i : nse)
+        //     cout<<i<<" ";
+        // cout<<"\nPSE : ";
+        // for(auto i : pse)
+        //     cout<<i<<" ";
+
+        int largestArea = 0;
+        
+        int n = heights.size();
+        for(int i = 0;i < n;i++)
+        {
+            int area = heights[i]*(nse[i] - pse[i] - 1);
+            largestArea = max(largestArea, area);
+        }
+        return largestArea;
+    }
+};
