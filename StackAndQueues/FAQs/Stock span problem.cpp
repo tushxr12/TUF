3d7d5305bc9b99ead67d0d13df494b8305ed8a00
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-02 11:37:45 +0530

    Practice stack problem

--- StackAndQueues/FAQs/Stock span problem.cpp
+++ StackAndQueues/FAQs/Stock span problem.cpp
@@ -19,3 +19,38 @@ class Solution
        return ans;
     }
 };
+
+//Optimal
+class Solution
+{
+    private:
+    vector<int> findPGE(vector<int> &arr, int n)
+    {
+        vector<int> pge(n);
+        stack<int> st;
+
+        for(int i = 0;i < n;i++)
+        {
+            while(!st.empty() && arr[st.top()] <= arr[i])
+                st.pop();
+            
+            if(st.empty())
+                pge[i] = -1;
+            else
+                pge[i] = st.top();
+            
+            st.push(i);
+        }
+        return pge;
+    }
+    public:
+    vector <int> stockSpan(vector<int> arr, int n) {
+       vector<int> pge = findPGE(arr, n);
+       vector<int> ans(n);
+       for(int i = 0;i < n;i++)
+       {
+         ans[i] = i - pge[i];
+       }
+       return ans;
+    }
+};
