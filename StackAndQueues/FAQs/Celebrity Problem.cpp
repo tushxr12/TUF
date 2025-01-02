d1ae4e512a7e69e514b527a465e7a87e96fae78f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-02 11:58:51 +0530

    Practice stack problem

--- StackAndQueues/FAQs/Celebrity Problem.cpp
+++ StackAndQueues/FAQs/Celebrity Problem.cpp
@@ -1,3 +1,4 @@
+//Brute force
 class Solution
 {
 public:
@@ -27,5 +28,51 @@ public:
             }
         }
         return ans;
+    }
+};
+
+//Optimal
+class Solution
+{
+public:
+    int celebrity(vector<vector<int>> &M){
+        int n = M.size();
+        int top = 0, down = n - 1;
+
+        while(top < down)
+        {
+            if(M[top][down] == 1)
+            {
+                top += 1;
+            }
+            else if(M[down][top] == 1)
+            {
+                down -= 1;
+            }
+            else
+            {
+                top += 1;
+                down -= 1;
+            }
+        }
+
+        if(top > down)
+        {
+            return -1;
+        }
+
+        for(int i = 0;i < n;i++)
+        {
+            if(i == top)
+                continue;
+            if(M[top][i] == 0 && M[i][top] == 1)
+            {
+                //Nothing to do
+            }
+            else
+                return -1;
+        }
+
+        return top;
     }
 };
