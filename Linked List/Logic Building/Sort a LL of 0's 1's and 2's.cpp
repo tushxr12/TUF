e7edf7e2b0f78befc454764b829535206f5db177
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-07 15:27:18 +0530

    practice linked list problem

--- Linked List/Logic Building/Sort a LL of 0's 1's and 2's.cpp
+++ Linked List/Logic Building/Sort a LL of 0's 1's and 2's.cpp
@@ -60,3 +60,72 @@ public:
         return head;
     }
 };
+
+//Optimal
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode* zeroHead = new ListNode(-1), *oneHead = new ListNode(-1), *twoHead = new ListNode(-1);
+        ListNode* zero = zeroHead, *one = oneHead, *two = twoHead;
+
+        ListNode* temp = head;
+
+        while(temp != NULL)
+        {
+            if(temp->val == 0)
+            {
+                zero->next = temp;
+                zero = zero->next;
+            }
+            else if(temp->val == 1)
+            {
+                one->next = temp;
+                one = one->next;
+            }
+            else 
+            {
+                two->next = temp;
+                two = two->next;
+            }
+            temp = temp->next;
+        }
+        
+        zero->next = (oneHead->next) ? oneHead->next : twoHead->next;
+        one->next = (twoHead->next);
+        two->next = NULL;
+        ListNode* newHead = zeroHead->next;
+
+        delete(oneHead);
+        delete(zeroHead);
+        delete(twoHead);
+
+        return newHead;
+    }
+};
