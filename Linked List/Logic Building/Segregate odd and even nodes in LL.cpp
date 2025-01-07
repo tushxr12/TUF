b47826736c7e173a31e11684406f6c0ab00e978d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-07 15:10:40 +0530

    practice linked list problem

---
+++ Linked List/Logic Building/Segregate odd and even nodes in LL.cpp
@@ -0,0 +1,59 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode* temp = head;
+        vector<int> v;
+
+        while(temp != NULL && temp->next != NULL)
+        {
+            v.push_back(temp->val);
+            temp = temp->next->next;
+        }
+        if(temp) 
+            v.push_back(temp->val);
+        temp = head->next;
+        while(temp != NULL && temp->next != NULL)
+        {
+            v.push_back(temp->val);
+            temp = temp->next->next;
+        }
+        if(temp) 
+            v.push_back(temp->val);
+        int i = 0;
+        temp = head;
+        while(temp)
+        {
+            temp->val = v[i];
+            i++;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
