c683424f2c6c027d3c992ca0efba8f8f6ce0519f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-08 18:19:42 +0530

    practice linked list problem

--- Linked List/Logic Building/Reverse a LL.cpp
+++ Linked List/Logic Building/Reverse a LL.cpp
@@ -22,6 +22,7 @@ struct ListNode
 };
 */
 
+//Brute (Modifying data directly)
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -42,5 +43,45 @@ public:
             temp = temp->next;
         }
         return head;
+    }
+};
+
+//Better (Modifying Links)
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head, *prev = NULL;
+        while(temp != NULL)
+        {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
     }
 };
