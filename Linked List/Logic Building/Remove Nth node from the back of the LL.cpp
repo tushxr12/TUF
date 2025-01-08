9d8a1d989dd0906ec134c5bdebc77cd281a61a7f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-08 17:50:46 +0530

    Practice linked-list problem

--- Linked List/Logic Building/Remove Nth node from the back of the LL.cpp
+++ Linked List/Logic Building/Remove Nth node from the back of the LL.cpp
@@ -58,3 +58,53 @@ class Solution {
         return head;
     }
 };
+
+//Better
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        for(int i = 0;i < n;i++)
+            fast = fast->next;
+        
+        if(fast == NULL)
+        {
+            return head->next;
+        }
+        
+        ListNode* slow = head;
+        while(fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete(delNode);
+        return head;
+    }
+};
