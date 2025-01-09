729483b17bcf341c295788f5ea139807bca9aed2
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-09 14:07:31 +0530

    practice linked list problem

--- Linked List/FAQs (Medium)/Check if LL is palindrome or not.cpp
+++ Linked List/FAQs (Medium)/Check if LL is palindrome or not.cpp
@@ -52,3 +52,68 @@ public:
         return false;
     }
 };
+
+
+//Optimized (Changing Links)
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+   private:
+    ListNode* reverseLL(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode *prev = NULL, *temp = head;
+        while (temp != NULL) {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+
+   public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* newHead = reverseLL(slow->next);
+        ListNode *first = head, *second = newHead;
+
+        while (second != NULL) {
+            if (first->val != second->val) {
+                reverseLL(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        reverseLL(newHead);
+        return true;
+    }
+};
