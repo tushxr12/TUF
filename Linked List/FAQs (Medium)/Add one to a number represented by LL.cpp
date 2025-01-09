843aa41103725cd03261452f3f606401176c2f2d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-09 13:50:50 +0530

    practice linked list problem

--- Linked List/FAQs (Medium)/Add one to a number represented by LL.cpp
+++ Linked List/FAQs (Medium)/Add one to a number represented by LL.cpp
@@ -64,3 +64,54 @@ class Solution {
         return origHead;
     }
 };
+
+//Recursive approach
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+   private:
+    int helper(ListNode* temp) {
+        if (temp == NULL) return 1;
+
+        int carry = helper(temp->next);
+        temp->val = temp->val + carry;
+        if (temp->val < 10) {
+            return 0;
+        }
+        temp->val = 0;
+        return 1;
+    }
+
+   public:
+    ListNode* addOne(ListNode* head) {
+        int carry = helper(head);
+        if (carry == 1) {
+            ListNode* newHead = new ListNode(carry);
+            newHead->next = head;
+            return newHead;
+        }
+        return head;
+    }
+};
