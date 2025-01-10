ca284c13e5fcbdd3d179a72deffa1d678281d94f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-10 19:27:25 +0530

    practice linked list problem

--- Linked List/FAQs (Medium)/Find the starting point in LL.cpp
+++ Linked List/FAQs (Medium)/Find the starting point in LL.cpp
@@ -36,3 +36,54 @@ class Solution {
         return NULL;
     }
 };
+
+//Optimal (Tortoise algo)
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode *findStartingPoint(ListNode *head) {
+        ListNode* slow = head, *fast = head;
+
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+            {
+                slow = head;
+                while(slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+
+        return NULL;
+    }
+};
