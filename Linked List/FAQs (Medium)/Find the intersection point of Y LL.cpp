86ed862d0a661cccdbaba8302eb841f6f18991f4
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-10 19:18:29 +0530

    practice linked list problem

--- Linked List/FAQs (Medium)/Find the intersection point of Y LL.cpp
+++ Linked List/FAQs (Medium)/Find the intersection point of Y LL.cpp
@@ -113,3 +113,49 @@ public:
         return NULL;
     }
 };
+
+//Optimal
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp1 = headA, *temp2 = headB;
+        while(temp1 != temp2)
+        {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+
+            if(temp1 == temp2)
+                return temp1;
+            
+            if(temp1 == NULL)
+                temp1 = headB;
+            if(temp2 == NULL)
+                temp2 = headA;
+        }
+        return NULL;
+    }
+};
