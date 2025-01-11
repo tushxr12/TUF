f190c55e3957d5903cc47aabd261b996ad40b55d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-11 21:43:30 +0530

    practice linked list problem

--- Linked List/FAQs (Medium)/Delete the middle node in LL.cpp
+++ Linked List/FAQs (Medium)/Delete the middle node in LL.cpp
@@ -56,3 +56,52 @@ public:
         return head;
     }
 };
+
+//Optimized
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(!head || !head->next)
+        {
+            delete head;
+            return nullptr;
+        }
+
+        ListNode* slow = head, *fast = head;
+        fast = fast->next->next;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* middle = slow->next;
+        slow->next = slow->next->next;
+        delete(middle);
+        return head;
+    }
+};
