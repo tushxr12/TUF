f4064cb900cba2bae29f610728d08fde37958f00
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-11 19:59:33 +0530

    practice linked list problem

--- Linked List/FAQs (Medium)/Length of loop in LL.cpp
+++ Linked List/FAQs (Medium)/Length of loop in LL.cpp
@@ -42,3 +42,58 @@ public:
         return 0l;
     }
 };
+
+//Optimized
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+private:
+    int findLength(ListNode* slow, ListNode* fast)
+    {
+        int length = 1;
+        fast = fast->next;
+        while(fast != slow)
+        {
+            fast = fast->next;
+            length++;
+        }
+        return length;
+    }
+public:
+    int findLengthOfLoop(ListNode *head) {
+        ListNode* slow = head, *fast = head;
+
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+            {
+                return findLength(slow, fast);
+            }
+        }
+        return 0;
+    }
+};
