3138ea56e2aaa9c74754999eafffaa37d809aca9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-15 16:40:34 +0530

    practice linked list problem

--- Linked List/FAQs (Hard)/Rotate a LL.cpp
+++ Linked List/FAQs (Hard)/Rotate a LL.cpp
@@ -44,3 +44,58 @@ public:
         return head;
     }
 };
+
+//Better
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode* temp = head;
+        int length = 1;
+        while(temp->next != NULL)
+        {
+            ++length;
+            temp = temp->next;
+        }
+
+        temp->next = head;
+        k %= length;
+        int diff = length - k;
+
+        while(diff-- > 0)
+        {
+            temp = temp->next;
+        }
+
+        head = temp->next;
+        temp->next = nullptr;
+
+        return head;
+    }
+};
