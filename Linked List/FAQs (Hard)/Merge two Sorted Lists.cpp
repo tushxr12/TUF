5299eb461175dce040a4d44d69417167d2b3601a
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-15 17:11:27 +0530

    practice linked list problem

--- Linked List/FAQs (Hard)/Merge two Sorted Lists.cpp
+++ Linked List/FAQs (Hard)/Merge two Sorted Lists.cpp
@@ -46,3 +46,56 @@ public:
         
     }
 };
+
+//Optimized
+// Definition of singly linked list:
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode(int data1)
+//     {
+//         val = data1;
+//         next = NULL;
+//     }
+//     ListNode(int data1, ListNode *next1)
+//     {
+//         val = data1;
+//         next = next1;
+//     }
+// };
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* t1 = list1, *t2 = list2;
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(t1 != NULL && t2 != NULL)
+        {
+            if(t1->val < t2->val)
+            {
+                temp->next = t1;
+                t1 = t1->next;
+            }
+            else
+            {
+                temp->next = t2;
+                t2 = t2->next;
+            }
+            temp = temp->next;
+        }
+
+        if(t1 != NULL)
+        {
+            temp->next = t1;
+        }
+
+        if(t2 != NULL)
+        {
+            temp->next = t2;
+        }
+        return dummyNode->next;
+    }
+};
