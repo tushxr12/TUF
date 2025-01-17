1d0fb9851821d7e13d2d27ca51f73ba9a9c0e431
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-17 15:13:22 +0530

    remove def

--- Linked List/FAQs (Medium)/Delete the middle node in LL.cpp
+++ Linked List/FAQs (Medium)/Delete the middle node in LL.cpp
@@ -1,27 +1,4 @@
-/*
-Definition of singly linked list:
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode()
-    {
-        val = 0;
-        next = NULL;
-    }
-    ListNode(int data1)
-    {
-        val = data1;
-        next = NULL;
-    }
-    ListNode(int data1, ListNode *next1)
-    {
-        val = data1;
-        next = next1;
-    }
-};
-*/
-
+//brute
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
