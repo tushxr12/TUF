11b291a340c6b2b08dc2d583d9d5ef7522282bb2
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-18 15:30:05 +0530

    practice linked list problem

--- Linked List/FAQs (Hard)/Flattening of LL.cpp
+++ Linked List/FAQs (Hard)/Flattening of LL.cpp
@@ -62,3 +62,74 @@ public:
         return arrToLinkedList(arr);
     }
 };
+
+//Optimized
+/*
+Definition of special linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *child;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        child = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        child = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode* next2)
+    {
+        val = data1;
+        next = next1;
+        child = next1;
+    }
+};
+*/
+
+class Solution {
+private:
+    ListNode* merge(ListNode* list1, ListNode* list2)
+    {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(list1 != NULL && list2 != NULL)
+        {
+            if(list1->val < list2->val)
+            {
+                temp->child = list1;
+                temp = list1;
+                list1 = list1->child;
+            }
+            else
+            {
+                temp->child = list2;
+                temp = list2;
+                list2 = list2->child;
+            }
+            temp->next = nullptr;
+        }
+        if(list1)
+            temp->child = list1;
+        if(list2)
+            temp->child = list2;
+        if(dummyNode->child)
+            dummyNode->child->next = nullptr;
+        return dummyNode->child;
+    }
+public:
+    ListNode* flattenLinkedList(ListNode* &head) {
+         if(head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode* mergeHead = flattenLinkedList(head->next);
+        head = merge(head, mergeHead);
+        return head;
+    }
+};
